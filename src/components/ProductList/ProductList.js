2853846790f9e42181630d73ff9f58aa7914b026
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2023-02-01 11:15:38 +0330

    add Product component in ProductList

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,7 +1,18 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Product from "./Product/Product";
+
 const ProductList = () => {
-  return ( 
-    <div>Products</div>
-   );
-}
- 
+  return (
+    <Container style={{ backgroundColor: "red" }}>
+      <Grid container spacing={3}>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+      </Grid>
+    </Container>
+  );
+};
+
 export default ProductList;
