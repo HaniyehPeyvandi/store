da3b9d04d0ce5781178338d81edca3991306e4ef
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2023-02-01 22:32:57 +0330

    add Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,24 +7,29 @@ import IconButton from "@mui/material/IconButton";
 import Badge from "@mui/material/Badge";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 import Container from "@mui/material/Container";
+import Drawer from "@mui/material/Drawer";
+import Cart from "../Cart/Cart";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Container>
-          <Toolbar>
+          <Toolbar sx={{ p: { sm: 0 } }}>
             <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
               Store
             </Typography>
 
-            <div>
-              <IconButton size="large" >
-                <Badge badgeContent={1} color="error">
-                  <ShoppingBagOutlinedIcon />
-                </Badge>
-              </IconButton>
-            </div>
+            <IconButton onClick={() => setOpen(true)}>
+              <Badge badgeContent={1} color="error">
+                <ShoppingBagOutlinedIcon />
+              </Badge>
+            </IconButton>
+            <Drawer open={open} anchor={"right"} onClose={() => setOpen(false)}>
+              <Cart />
+            </Drawer>
           </Toolbar>
         </Container>
       </AppBar>
