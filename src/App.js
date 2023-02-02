86a1fc64fc6f30bb50d980d51088553e12320d1f
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2023-02-03 01:33:48 +0330

    add the redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Home from './pages/Home/Home';
+import "./App.css";
+import Home from "./pages/Home/Home";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Home />
+      </div>
+    </Provider>
   );
 }
 
