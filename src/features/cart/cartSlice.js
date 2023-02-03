336efeacf623f30c87222f8c91cba53ddf123a98
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2023-02-03 15:41:47 +0330

    add totalCount and totalPrice to the store

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: { items: [] },
+  initialState: { items: [], totalCount: 0, totalPrice: 0 },
   reducers: {
     addToCart: (state, action) => {
       const itemInCart = state.items.find(
@@ -10,19 +10,19 @@ const cartSlice = createSlice({
       );
       if (itemInCart) {
         itemInCart.quantity++;
+        state.totalCount++;
+        state.totalPrice += itemInCart.price;
       } else {
         state.items.push({ ...action.payload, quantity: 1 });
+        state.totalCount++;
+        state.totalPrice += action.payload.price;
       }
     },
     incrementQuantity: (state, action) => {
       const item = state.items.find((item) => item.id === action.payload);
       item.quantity++;
-      // state.items = state.items.map((item) => {
-      //   if (item.id === action.payload) {
-      //     return { ...item, quantity: item.quantity + 1 };
-      //   }
-      //   return item;
-      // });
+      state.totalCount++;
+      state.totalPrice += item.price;
     },
     decrementQuantity: (state, action) => {
       const item = state.items.find((item) => item.id === action.payload);
@@ -31,15 +31,22 @@ const cartSlice = createSlice({
           (item) => item.id !== action.payload
         );
         state.items = filteredItems;
+        state.totalCount--;
+        state.totalPrice -= item.price;
       } else {
         item.quantity--;
+        state.totalCount--;
+        state.totalPrice -= item.price;
       }
     },
     removeItem: (state, action) => {
+      const item = state.items.find((item) => item.id === action.payload);
       const filteredItems = state.items.filter(
         (item) => item.id !== action.payload
       );
       state.items = filteredItems;
+      state.totalCount -= item.quantity;
+      state.totalPrice -= item.price * item.quantity;
     },
   },
 });
